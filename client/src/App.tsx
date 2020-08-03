2b7cc23d244f1d3e0ded7fb1f3f86c8d329a6ba7
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-08-03 14:26:07 +0200

    basic menu routes done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header'
+import Menu from './components/menu'
+import AppRouter from './routes/app.routes'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Menu/>
+      <AppRouter/>
     </div>
   );
 }
--- client/src/routes/app.routes.tsx
+++ client/src/routes/app.routes.tsx
@@ -22,3 +22,5 @@ function AppRouter(){
         </Switch>
     )
 }
+
+export default AppRouter
