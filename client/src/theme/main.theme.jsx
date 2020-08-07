176cedc334f05d0959e3f327c984e2207a2f061d
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-08-07 13:15:48 +0200

    started some theme. some guessing

---
+++ client/src/theme/main.theme.jsx
@@ -0,0 +1,14 @@
+export const theme = {
+    textColors: {
+        primary: '#1E1E1F',
+        secondary: '#44DAD7',   
+    },
+
+    box: {
+        boarder: '2rem solid #E5CB9F',
+        inner: '#F7EDDE',
+        text: '#1E1E1F',
+        padding: '1rem 2rem',
+        shadow: '5px 5px #1E1E1F'
+    }
+}
